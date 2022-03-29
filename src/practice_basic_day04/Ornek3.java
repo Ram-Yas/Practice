da997fb5b56e5ed8605a17b597419836bbd310e2
Ram-Yas <101016089+Ram-Yas@users.noreply.github.com>
2022-03-29 16:09:47 +0200

    Add files via upload
    29/03

---
+++ src/practice_basic_day04/Ornek3.java
@@ -0,0 +1,24 @@
+package practice_basic_day04;
+public class Ornek3 {
+    /*
+     *  String 2D array olustur
+     *  {{"$12" , "$22" , "$0"},   {"€9" , "€40" , "$1" },  {"€12", "$2","$0"}}
+     *  String de $ varsa 3.2 ile carp
+     *  String de € varsa 4.2 ile carp
+     *  elemanlarin toplamini double olarak yazdir
+     */
+    public static void main(String[] args) {
+        String array[][] = {{"$12" , "$22" , "$0"},   {"€9" , "€40" , "$1" },  {"€12", "$2","$0"}};
+        double toplam= 0;
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array [i].length; j++){
+                if(array[i][j].contains("$")){
+                    toplam += Double.parseDouble((array[i][j].replace("$", "")))*3.2;
+                }else if(array[i][j].contains("€")){
+                    toplam += Double.parseDouble((array[i][j].replace("€", "")))*4.2;
+                }
+            }
+        }
+        System.out.println("arraydaki elemanlarin toplami : "+toplam);
+    }
+}
