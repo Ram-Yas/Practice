f147557ed186ae105f37b8f3818ccaf6aed5a430
Ramazan Yasar <ramazanyas@gmail.com>
2022-04-11 14:43:44 +0200

    11/04/2022

--- src/interviewQuestions3/Q01_ReverseString.java
+++ src/interviewQuestions3/Q01_ReverseString.java
@@ -10,22 +10,52 @@ public class Q01_ReverseString {
 
     public static void main(String[] args) {
 
-        //2.yol
-
+        //1.Yol: StringBuilder () kullanarak
         String str = "Hayat paylasinca guzel";
+        StringBuilder strBuilder = new StringBuilder();
+        strBuilder.append(str); // bu method Stringbilder'a ekleme yapar
+
+        String revesedStr = strBuilder.reverse().toString();
+        System.out.println("1.yol: " + revesedStr); //1; yol: lezug acnisalyap tayaH
+
+        //2.Yol: String Classini kullanarak
+        System.out.print("2.yol :");
+
+        for (int i = str.length() - 1; i >= 0; i--) {//start from end of the string and got to the beginning
+            System.out.print(str.charAt(i));//her bir karakteri yazdir
+        }
+        System.out.println(); //alt satir ile karismasin diye
+
+        //4. yol : String Classini kullanarak
+
         String arr[] = str.split("");
-        String revesedStr = "";
+        revesedStr = "";
         for (int i = arr.length - 1; i >= 0; i--) {
             revesedStr += arr[i];
         }
-        System.out.println(revesedStr);
+        System.out.println("4.yol: " + revesedStr);
 
-        //3.yol
+        //3.Yol: Bir method olusturun, ardindan methodu main method dan cagirin
 
-        System.out.println(reversedStr(arr, revesedStr));
+        System.out.println("3.yol: " + reversedStr(arr, revesedStr));
+
+        //5.yol
+
+        System.out.print("5.yol: ");
+        strTersArray(str);
 
 
     }//main sonu
+
+    private static void strTersArray(String str) {
+        char[] arr2 = str.toCharArray(); //String bir veriyi karakterlere char tipinde ayirir
+                                          //split metoduna benziyor
+        for (int i = str.length() - 1; i >= 0; i--) {
+
+            System.out.print(arr2[i]);
+
+        }
+    }//end of the method
 
     private static String reversedStr(String[] arr, String revesedStr) {
         revesedStr = "";
