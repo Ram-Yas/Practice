7179780ae7cff24dce7013b732438ac37498d74e
Ramazan Yasar <ramazanyas@gmail.com>
2022-04-12 22:17:14 +0200

    12/04/2022

--- src/interviewQuestions4/Q02_CreateRandomList_SetList.java
+++ src/interviewQuestions4/Q02_CreateRandomList_SetList.java
@@ -1,5 +1,7 @@
 package interviewQuestions4;
 
+import java.util.ArrayList;
+
 public class Q02_CreateRandomList_SetList {
     // int array list oluşturun 10 elemandan oluşmalı
     // random ekle isinde void bir metod oluşturun  ve parametresi int array list olsun.
@@ -7,5 +9,34 @@ public class Q02_CreateRandomList_SetList {
     // eğer çift sayı atarsa o elemaının yerine 111 yazsın
     // eğer çift sayı yoksa çift sayı yoktur mesajını versin.
 
+    public static void main(String[] args) {
+
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        randomEkle(list);
+
+    }
+
+    private static void randomEkle(ArrayList<Integer> list) {
+        for (int i = 0; i < 10; i++) {
+            list.add((int) (Math.random()*20)); //double veri tipine sahip olan Math.random class'i hata verdiginden
+                                                //integer'a casting yaptik.. random() methodu 0-1 arasi (double) sayi uretir ..
+
+        }
+        System.out.println("random liste : "+list);
+
+        int count = 0;
+        for (int i = 0; i <list.size(); i++) { //random olusturulan elemanlarin  listesine bakiyoruz
+            if (list.get(i)%2==0){// cift sayi varsa
+                count++;
+                list.set(i,111);
+            }
+        }
+        System.out.println("111'ler eklendikten sonra random liste : "+list);
+
+        if (count==0) System.out.println("Listede cift sayi yoktur..");
+
+        System.out.println("Listenin son hali :"+list);
+
+    }
 
 }
--- src/interviewQuestions4/Q03_HackerLanguage.java
+++ src/interviewQuestions4/Q03_HackerLanguage.java
@@ -1,22 +1,66 @@
 package interviewQuestions4;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Q03_HackerLanguage {
 
-// Kullanıcının yazdığı metni, 'hacker'ların konuşma diline çeviren bir method(method ismi hackerDili) yazınız.
-//    Hackerlar bazı harfleri sayılara çevirerek yazışabiliyorlar. Genellikle çevirdikleri harfler şu şekilde:
-//    s -> 5
-//    a -> 4
-//    e -> 3
-//    i -> 1
-//    o -> 0
-//    Test data
-//    hackerDili("java ile hersey guzel")
-//    j4v4 1l3 h3r53y guz3l
-//    İpucu harfleri değiştirin ve ekrana yazdırın.
+    /*
+    Kullanıcının yazdığı metni, 'hacker'ların konuşma diline çeviren bir method(method ismi hackerDili) yazınız.
+   Hackerlar bazı harfleri sayılara çevirerek yazışabiliyorlar. Genellikle çevirdikleri harfler şu şekilde:
+   s -> 5
+   a -> 4
+   e -> 3
+   i -> 1
+   o -> 0
+   Test data
+   hackerDili("java ile hersey guzel")
+   j4v4 1l3 h3r53y guz3l
+   İpucu harfleri değiştirin ve ekrana yazdırın.
+
+     */
 
 
 
     public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("bir cumle giriniz : ");
+        String sentence = scanner.nextLine();
+
+        hackerDili(sentence);
+        
+    }
+
+    private static void hackerDili(String sentence) {
+
+        String arr[] = new String[sentence.length()];
+
+        for (int i = 0; i <sentence.length(); i++){
+            arr[i]= sentence.substring(i,i+1); //substring'i karakteri almak icin kullandik
+            System.out.print(arr[i]);
+        }
+        System.out.println(); //bundan sonraki satirda gelenler birlesmesinler diye sout attik
+
+        for (int i = 0; i < sentence.length(); i++) {
+            if (arr[i].contains("s")){
+                arr[i]="5";
+            }
+            if (arr[i].contains("a")){
+                arr[i]="4";
+            }
+            if (arr[i].contains("e")){
+                arr[i]="3";
+            }
+            if (arr[i].contains("i")){
+                arr[i]="1";
+            }
+            if (arr[i].contains("o")){
+                arr[i]="0";
+            }
+            System.out.print(arr[i]);
+        }
+
 
     }
 }
