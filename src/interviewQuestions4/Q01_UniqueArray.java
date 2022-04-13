2f559bb18d5e43faf9b3aae61db1bac6dda8356a
Ramazan Yasar <ramazanyas@gmail.com>
2022-04-13 23:19:41 +0200

    13/04/2022

--- src/interviewQuestions4/Q01_UniqueArray.java
+++ src/interviewQuestions4/Q01_UniqueArray.java
@@ -12,17 +12,17 @@ public class Q01_UniqueArray {
      * OUTPUT : [1, 2, 3, 4, -2, 5, 6, 8, 7, 9, 10]
      */
 
-
     public static void main(String[] args) {
+        int arr [] = {1,2,2,3,1,4,2,-2,5,6,8,7,5,6,-2,9,3,10};
 
-        int arr[] = {1, 2, 2, 3, 1, 4, 2, -2, 5, 6, 8, 7, 5, 6, -2, 9, 3, 10};
-        List<Integer> tekrarsizListe = new ArrayList<Integer>();
+        List<Integer> tekrarsizListe = new ArrayList<>();
 
         for (int i = 0; i < arr.length; i++) {
-            if (!tekrarsizListe.contains(arr[i])) {
+            if(!tekrarsizListe.contains(arr[i])){
                 tekrarsizListe.add(arr[i]);
             }
         }
-        System.out.println(tekrarsizListe);
+        System.out.println(tekrarsizListe); //[1, 2, 3, 4, -2, 5, 6, 8, 7, 9, 10]
     }
+
 }
--- src/interviewQuestions4/Q06_MultipleMethodVarargs.java
+++ src/interviewQuestions4/Q06_MultipleMethodVarargs.java
@@ -7,5 +7,8 @@ public class Q06_MultipleMethodVarargs {
 	yazdıran parametreli method yazınız
  */
 
+    public static void main(String[] args) {
+
+    }
 
 }
