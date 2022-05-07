7a5cc5671adf9aa1557b8209b4444349f6bb8af7
Ramazan Yasar <ramazanyas@gmail.com>
2022-05-07 16:07:57 +0200

    07/05/2022

--- src/interviewQuestions7/Q01_UseContinue.java
+++ src/interviewQuestions7/Q01_UseContinue.java
@@ -10,7 +10,20 @@ public class Q01_UseContinue {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-        String str= scan.nextLine();
+        System.out.print("Bir string giriniz : ");
+        String ifade= scan.nextLine();
+
+        for (int i = 0; i <ifade.length(); i++) { // ifadedeki butun karakterlere bakacak
+            if (ifade.charAt(i)=='a' || ifade.charAt(i)==' ') // ifade'de a ye da bosluk icerirse,
+                continue;   // bu satir calistiginda dongunun basina gide,
+                            // cunku a veya bosluk var iken herhangi bir ciktisin istemiyor
+                            // breal aklimiza gelebilir.. ancak break kodu tamamen kirar ve devam etmez..
+            System.out.println(ifade.charAt(i));
+
+        }
+
+        //continue ile break arasindaki fark, break donguyu kiriyor, kapatiyor.
+        // continue ise donguyu kirmaz, dongunun basina alir
 
     }
 }
--- src/interviewQuestions7/Q02_TreeShape.java
+++ src/interviewQuestions7/Q02_TreeShape.java
@@ -1,8 +1,25 @@
 package interviewQuestions7;
 
 public class Q02_TreeShape {
-// Dikey olarak bolunen bir agac sekli ciziniz.. agacin tac uzunlugunu kullanicidan aliniz..
-// Not :govde sabit sayi olsun
-    /* tac uzunlugu -> 6 iken
-     */
+        // Dikey olarak bolunen bir agac sekli ciziniz.. agacin tac uzunlugunu kullanicidan aliniz..
+        // Not :govde sabit sayi olsun
+            /* tac uzunlugu -> 6 iken
+             *
+
+               ^
+               ^^
+               ^^^
+               ^^^^
+               ^^^^^
+               ^^^^^^
+               |||
+               |||
+               |||
+               |||
+               |||
+               */
+        public static void main(String[] args) {
+
+
+        }
 }
